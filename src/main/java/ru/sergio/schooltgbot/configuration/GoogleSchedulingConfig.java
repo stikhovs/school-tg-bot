0b4cd88a9d74f1ba7bc72774fcd0368653c26f19
Sergei Tikhov <s.tikhov@unabank.com>
2022-02-07 23:46:30 +0300

    Ping Google to not fall asleep

---
+++ src/main/java/ru/sergio/schooltgbot/configuration/GoogleSchedulingConfig.java
@@ -0,0 +1,27 @@
+package ru.sergio.schooltgbot.configuration;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+public class GoogleSchedulingConfig {
+
+    @SneakyThrows
+    @Scheduled(fixedRate = 1500000L) // 25 min
+    public void pingGoogle() {
+            URL url = new URL("https://www.google.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            log.info("Ping {}, OK: response code {}", url.getHost(), connection.getResponseCode());
+            connection.disconnect();
+    }
+
+}
