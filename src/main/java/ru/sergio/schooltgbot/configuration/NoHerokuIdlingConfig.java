ac38ea14d13af8e0db59615a38fac65255d1aab5
Sergei Tikhov <s.tikhov@unabank.com>
2022-02-12 22:27:53 +0300

    Add NoHerokuIdlingConfig

---
+++ src/main/java/ru/sergio/schooltgbot/configuration/NoHerokuIdlingConfig.java
@@ -0,0 +1,23 @@
+package ru.sergio.schooltgbot.configuration;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+public class NoHerokuIdlingConfig {
+
+    @SneakyThrows
+    @Scheduled(fixedRate = 1200000L) // 20 min
+    public void pingGoogle() {
+        RestTemplate restTemplate = new RestTemplate();
+        Object obj = restTemplate.getForObject("https://school-tg-bot.herokuapp.com/actuator/health", Object.class);
+        log.info("Self ping {}", obj);
+    }
+
+}
